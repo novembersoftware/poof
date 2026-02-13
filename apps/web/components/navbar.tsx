ef1a9a27d9df2be7ca2c4cb401df4b2ac52e99a9
Kyle Dickey <k@kyle.so>
2026-02-12 18:52:11 -0700

    add working dashboard link to navbar

--- apps/web/components/navbar.tsx
+++ apps/web/components/navbar.tsx
@@ -1,20 +1,24 @@
 "use client";
 
+import { authClient } from "@/lib/auth-client";
 import { Authenticated, Unauthenticated } from "convex/react";
 import { AsteriskIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 
 export function Navbar() {
-    // const data = useQuery(api.auth.getCurrentUserAndOrgs);
-    // console.log(data);
+    const { data: organizations } = authClient.useListOrganizations();
     return (
         <nav className="fixed top-0 flex h-14 w-full items-center justify-between px-4 sm:px-8 md:px-20">
             <Link href="/">
                 <AsteriskIcon className="mr-1 inline-block size-6" />
             </Link>
             <Authenticated>
-                <Button nativeButton={false} size="sm" render={<Link href="/dashboard" />}>
+                <Button
+                    nativeButton={false}
+                    size="sm"
+                    render={<Link href={`/${organizations?.[0]?.slug}`} />}
+                >
                     Dashboard
                 </Button>
             </Authenticated>
