5cb7cb3306cf3a6d7e652a5fe123e7a9d4b49fcc
Kyle Dickey <k@kyle.so>
2026-05-27 15:49:56 -0600

    fix broken server

--- src/server.tsx
+++
@@ -1,9 +0,0 @@
-import { createStartHandler, defaultRenderHandler } from "@tanstack/react-start/server";
-import type { Register } from "@tanstack/react-router";
-import type { RequestHandler } from "@tanstack/react-start/server";
-
-export const fetch = createStartHandler(defaultRenderHandler);
-
-type ServerEntry = { fetch: RequestHandler<Register> };
-
-export default { fetch } satisfies ServerEntry;
